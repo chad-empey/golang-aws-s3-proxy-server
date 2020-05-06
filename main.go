bce4fdbebcea54dd4d6befae7831846c8c34da9a
Chad Joseph Empey <chadmp@churchofjesuschrist.org>
2020-05-06 16:40:00 -0600

    support ranges for videos

--- main.go
+++ main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
-	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -18,16 +19,47 @@ var svc *s3.S3
 var bucket string
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Cache-Control", "no-cache")
 	path := strings.Replace(r.URL.Path, "/", "", 1)
+	value := 0
 
-	req, _ := svc.GetObjectRequest(&s3.GetObjectInput{
+	requestRange := r.Header.Get("Range")
+
+	if requestRange != "" {
+		split := strings.Split(requestRange, "=")
+		next := strings.Split(split[1], "-")
+		raw := next[0]
+		value, _ = strconv.Atoi(raw)
+	}
+
+	meta, err := svc.HeadObject(&s3.HeadObjectInput{
 		Bucket: aws.String(bucket),
 		Key:    aws.String(path),
 	})
 
-	url, _ := req.Presign(20 * time.Minute)
-	http.Redirect(w, r, url, 302)
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	output, err := svc.GetObject(&s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(path),
+		Range:  aws.String(fmt.Sprintf("bytes=%d-%d", value, *meta.ContentLength)),
+	})
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Add("Accept-Ranges", "bytes")
+	w.Header().Add("Content-Length", strconv.FormatInt(*output.ContentLength, 10))
+
+	if value > 0 {
+		w.Header().Add("Content-Range", fmt.Sprintf("bytes %d-%d/%d", value, *meta.ContentLength-1, *meta.ContentLength))
+		w.WriteHeader(206)
+	}
+
+	io.Copy(w, output.Body)
 }
 
 func main() {
